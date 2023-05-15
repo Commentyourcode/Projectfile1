5f1a159bf9259eda78aa6cbac14ce035c375ec3a
José Pedro Cassiano <jpcassiano11@gmail.com>
2023-05-15 17:00:43 -0300

    CSS changes
    Mainly CSS chages to adapt the img button that will be used as a link as in the previous project. Failed attempts to change the backrgound color of the links led to an inconsistency in the code, but there are options that have not been explored yet, such as javascript usage. Link javascript and elaborate with  further css changes.

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+var mainbtn = document.getElementsByClassName("mainbtn")
+var droplink1 = document.getElementsByClassName("link1")
+var droplink2 = document.getElementsByClassName("link2")
+var droplink3 = document.getElementsByClassName("link3")
+var droplink4 = document.getElementsByClassName("link4")
