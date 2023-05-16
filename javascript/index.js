6e4a5defef8dbaf632ec22195d206cbc4ce48e32
José Pedro Cassiano <jpcassiano11@gmail.com>
2023-05-16 17:09:59 -0300

    Another day, another bug
    Designed a bit more of the header and adjusted the new imperial logo. Also tried adding a font, but for some reason, i can't link it and the Visual Studio doesn't identify anyway. investigate the bug and fix for this project and as reference for future projects.

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,4 @@ var droplink1 = document.getElementsByClassName("link1")
 var droplink2 = document.getElementsByClassName("link2")
 var droplink3 = document.getElementsByClassName("link3")
 var droplink4 = document.getElementsByClassName("link4")
+
