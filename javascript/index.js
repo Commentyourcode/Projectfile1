e8a62a26fc9d8256af0bf4ebb4ff16d196d37afe
José Pedro Cassiano <jpcassiano11@gmail.com>
2023-05-19 17:18:29 -0300

    I had an idea
    Just finished the history page and i had a stupid and insane idea, but great (and it works). Use a hologram to exhibit the picture of  a sith beside the text. How you will come up with it? no idea, figure it out and good luck.

--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-var mainbtn = document.getElementsByClassName("mainbtn")
-var droplink1 = document.getElementsByClassName("link1")
-var droplink2 = document.getElementsByClassName("link2")
-var droplink3 = document.getElementsByClassName("link3")
-var droplink4 = document.getElementsByClassName("link4")
-
